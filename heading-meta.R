56f0d154fa5ca40edb09a0129a95a90b4614b00d
Henrik Bengtsson <henrik.bengtsson@gmail.com>
2025-12-03 14:33:16 -0800

    Update metadata info

--- heading-meta.R
+++ heading-meta.R
@@ -1,10 +1,10 @@
-metathis::meta() |> 
+metathis::meta() |>
   metathis::meta_social(
-    title = "Futureverse P2P: Peer-to-Peer Parallelization in R",
-    description = "An 18-minute talk titled 'Futureverse P2P: Peer-to-Peer Parallelization in R - Share compute among friends across the world",
-    url = "https://henrikbengtsson.github.io/talk-user2025-p2p/#/",
-    image = "https://raw.githubusercontent.com/HenrikBengtsson/talk-user2025-p2p/main/images/index-img.png",
-    image_alt = "The title slide of the presentation, with author information, the future logo and an image of the world globe with three connected peer-to-peer users",
+    title = "future.p2p: Peer-to-Peer Compute Clusters in R",
+    description = "An 10-minute talk titled 'future.p2p: Peer-to-Peer Compute Clusters in R",
+    url = "https://henrikbengtsson.github.io/talk-latinr2025-p2p/#/",
+    image = "https://raw.githubusercontent.com/HenrikBengtsson/talk-latinr2025-p2p/main/images/index-img.png",
+    image_alt = "Title slide for a talk titled 'future.p2p: Peer-to-Peer Compute Clusters in R – Share compute among friends.' Presented by Henrik Bengtsson, University of California, San Francisco; R Foundation; R Consortium, with social icons and the handle @HenrikBengtsson. On the right side, two images: a hexagonal 'Futureverse' logo with a retro-futuristic design, and an illustration of three people using laptops while connected across a world map with location pins forming a network. Footer shows ‘LatinR 2025 (2025W49)’ and a link to future.p2p.futureverse.org.",
     twitter_card_type = "summary_large_image",
     twitter_creator = "@HenrikBengtsson"
   )
